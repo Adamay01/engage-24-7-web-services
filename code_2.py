b578cbcd365ba2e50d472856611677a74cda54d5
Ada <139880994+Adamay01@users.noreply.github.com>
2023-11-25 21:12:28 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,41 +1,17 @@
 
 
-        }
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-            System.out.println("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
 }
-    }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        }    const data = [];
-    }
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
 }
-
-    });
+    return data;
+    data.forEach(item => {
 main();
 function main() {
-    data.forEach(item => {
-    for (let i = 0; i < 10; i++) {
 function generateRandomData() {
 
-        data.push(Math.floor(Math.random() * 100) + 1);
         console.log("Random Number: " + item);
-    const data = generateRandomData();
-}
-
-    return data;    }
-        }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
-            System.out.println("Random Number: " + item);
-
-        }
-}
-        int[] data = new int[10];
+    });
+    const data = [];
+    }
